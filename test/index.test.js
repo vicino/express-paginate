b2cc6324a65f9b29e308a2b282a6834eb0164709
Alois Barreras <alois@vicino.io>
2017-06-23 23:44:20 -0400

    added some tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,71 @@
+const paginate = require('../lib/index');
+const { assert } = require('chai');
 
-var paginate = require('../lib/index');
+describe('express-paginate', () => {
+  let req;
 
-describe('express-paginate', function () {
+  beforeEach(() => {
+    req = {
+      query: {
+        page: '2',
+        limit: '20',
+      },
+    };
+  });
 
+  it('should use default values of page=1 and limit=10', () => {
+    delete req.query.page;
+    delete req.query.limit;
+
+    const middleware = paginate();
+    middleware(req, {}, () => { });
+    assert.equal(req.query.page, 1);
+    assert.equal(req.query.limit, 10);
+    assert.equal(req.skip, 0);
+  });
+
+  it('should enforce default maxLimit of 50', () => {
+    req.query.limit = '100';
+    const middleware = paginate();
+    middleware(req, {}, () => { });
+    assert.equal(req.query.limit, 50);
+    assert.equal(req.skip, 50);
+  });
+
+  it('should enforce custom maxLimit', () => {
+    req.query.limit = '100';
+    const middleware = paginate('10', '75');
+    middleware(req, {}, () => { });
+    assert.equal(req.query.limit, 75);
+    assert.equal(req.skip, 75);
+  });
+
+  it('should enforce page >= 1 and default to 1', () => {
+    req.query.page = '-100';
+    const middleware = paginate();
+    middleware(req, {}, () => { });
+    assert.equal(req.query.page, 1);
+  });
+
+  it('should enforce limit > 0 and default to 10', () => {
+    req.query.limit = '-100';
+    const middleware = paginate();
+    middleware(req, {}, () => { });
+    assert.equal(req.query.limit, 10);
+    assert.equal(req.skip, 10);
+  });
+
+  it('should enforce limit > 0 and use custom limit', () => {
+    req.query.limit = '-100';
+    const middleware = paginate(75);
+    middleware(req, {}, () => { });
+    assert.equal(req.query.limit, 75);
+    assert.equal(req.skip, 75);
+  });
+
+  it('should call next', (done) => {
+    req.query.limit = '-100';
+    const middleware = paginate(75);
+    middleware(req, {}, done);
+  });
 });
